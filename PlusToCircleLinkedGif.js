eb9183df3db3606ec2ed0d015515f1c7faabc183
Anwesh43 <anweshthecool0@gmail.com>
2018-08-17 18:16:25 +0530

    adding State class

---
+++ PlusToCircleLinkedGif.js
@@ -0,0 +1,21 @@
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating() {
+        this.dir = 1 - 2 * this.prevScale
+    }
+}
